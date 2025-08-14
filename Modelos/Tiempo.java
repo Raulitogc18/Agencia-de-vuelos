532a91be746f4c53504b80127375f6846cdf0e2b
Raulitogc18 <raulgodinezcabrera@gmail.com>
2025-08-14 09:19:14 -0600

    Cambio_Arreglo_doble_tiempo

--- Modelos/Tiempo.java
+++ Modelos/Tiempo.java
@@ -7,26 +7,49 @@ public class Tiempo {
     private final String hora;
     private final String fecha;
 
+    // Arreglo doble para almacenar múltiples horas y fechas
+    private static String[][] registros;
+
     // Constructor de la clase
     public Tiempo(String hora, String fecha) {
         this.hora = hora;
         this.fecha = fecha;
     }
 
+    // Inicializar el arreglo doble con tamaño dado
+    public static void inicializarRegistros(int filas, int columnas) {
+        registros = new String[filas][columnas];
+    }
+
+    // Guardar un tiempo en el arreglo doble
+    public static void guardarRegistro(int fila, int columna, String valor) {
+        if (registros != null && fila < registros.length && columna < registros[0].length) {
+            registros[fila][columna] = valor;
+        }
+    }
+
+    // Mostrar todo el arreglo doble
+    public static void mostrarRegistros() {
+        if (registros != null) {
+            for (int i = 0; i < registros.length; i++) {
+                for (int j = 0; j < registros[i].length; j++) {
+                    System.out.print(registros[i][j] + "\t");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("El arreglo de registros no ha sido inicializado.");
+        }
+    }
 
     // Métodos de la clase
-    public String setHora(String hora) {
+    public String getHora() {
         return hora;
     }
 
-    public String setFecha(String fecha) {
+    public String getFecha() {
         return fecha;
     }
-
-    // Eliminar método incompleto o implementarlo correctamente
-    // public int setDias(String dias) {
-    //     // Implementación aquí
-    // }
 
     public void mostrarTiempo() {
         System.out.println("Hora: " + hora + ", Fecha: " + fecha);
